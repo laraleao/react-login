822424f1e198c591a03f0a203f412dd75602875e
Lara Leão <laraleao@hotmail.com>
2021-10-19 21:27:23 -0300

    atualizando front

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, TouchableOpacity, Button, StyleSheet} from 'react-native';
+
+class ButtonClick extends Component {
+  _onPressButton() {}
+}
 
 const Home = ({navigation}) => {
   return (
-    <View>
-      <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+    <View
+      style={{
+        backgroundColor: 'lightblue',
+        padding: 10,
+        flex: 1,
+        justifyContent: 'center',
+      }}>
+      {/* <TouchableOpacity onPress={() => navigation.navigate('Login')}>
         <Text style={{color: 'black'}}>Login</Text>
-        <TextInput>Digite seu e-mail</TextInput>
       </TouchableOpacity>
 
       <TouchableOpacity onPress={() => navigation.navigate('Cadastro')}>
         <Text style={{color: 'black'}}>Cadastro</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
+
+      <Button onPress={() => this._onPressButton} title="Login" />
+      <Button onPress={() => this._onPressButton} title="Cadastro" />
     </View>
   );
 };
